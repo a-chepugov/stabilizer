b5e1809e2527bfc5c353f52f3afcecf582e74449
a.chepugov <a.chepugov@gmail.com>
2026-03-14 23:37:04 +0200

    Refactor Utils.h: Move linear interpolation and add RMS calculation functionality to new utils.h file, improving organization and code clarity.

--- firmware/Utils.h
+++
@@ -1,4 +0,0 @@
-// Линейная интерполяция положения точки как процент от длины AB (0..1)
-constexpr float spacing(float a, float b, float passed) {
-    return a * (1 - passed) + b * passed;
-}  
---
+++ firmware/utils.h
@@ -0,0 +1,21 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// Линейная интерполяция положения точки как процент от длины AB (0..1)
+constexpr float spacing(float a, float b, float passed) {
+    return a * (1 - passed) + b * passed;
+}
+
+uint16_t rms(const uint16_t* data, size_t length) {
+    size_t count = 0;
+    uint32_t sum = 0;
+    for (size_t i = 0; i < length; i++) {
+        if (data[i] > 0) {
+            count++;
+            sum += (uint32_t)data[i] * (uint32_t)data[i];           
+        }
+    }
+    return (uint16_t) (sqrt(sum / count));
+}
+
+#endif
