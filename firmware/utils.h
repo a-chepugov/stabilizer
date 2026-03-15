a68635b249f8194575d35efcc331399eeda75f52
a.chepugov <a.chepugov@gmail.com>
2026-03-16 00:48:54 +0200

    Fix RMS calculation in utils.h: Update the function to compute the root mean square using the total length of the data array instead of counting positive values, improving accuracy. Clean up code by removing unnecessary variables.

--- firmware/utils.h
+++ firmware/utils.h
@@ -8,16 +8,13 @@ namespace utils {
     }
 
     uint16_t rms(const uint16_t* data, size_t length) {
-        size_t count = 0;
         uint32_t sum = 0;
         for (size_t i = 0; i < length; i++) {
-            if (data[i] > 0) {
-                count++;
-                sum += (uint32_t)data[i] * (uint32_t)data[i];           
-            }
+            sum += (uint32_t)data[i] * (uint32_t)data[i];
         }
-        return (uint16_t) (sqrt(sum / count));
+        return (uint16_t) (sqrt(sum / length));
     }
+
 }
 
 #endif
