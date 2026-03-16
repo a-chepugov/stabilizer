28e031db01843cbe503ac6b724490ce9c74617dc
a.chepugov <a.chepugov@gmail.com>
2026-03-16 21:27:02 +0200

    Refactor utils.hpp: Rename parameter in spacing function for clarity and change rms function to constexpr for improved performance.

--- firmware/utils.hpp
+++ firmware/utils.hpp
@@ -2,11 +2,11 @@
 
 namespace utils {
     // Линейная интерполяция положения точки как процент от длины AB (0..1)
-    constexpr float spacing(float a, float b, float passed) {
-        return a * (1 - passed) + b * passed;
+    constexpr float spacing(float a, float b, float part) {
+        return a * (1 - part) + b * part;
     }
 
-    uint16_t rms(const uint16_t* data, size_t length) {
+    constexpr uint16_t rms(const uint16_t* data, size_t length) {
         uint32_t sum = 0;
         for (size_t i = 0; i < length; i++) {
             sum += (uint32_t)data[i] * (uint32_t)data[i];
