9e375feebf02843270bd85a3cd3c911b22b0f2b1
a.chepugov <a.chepugov@gmail.com>
2026-03-17 01:21:55 +0200

    Refactor Periods.hpp: Update data structures to improve performance and organization by changing raw buffer type to uint32_t, introducing a new rms function for RMS calculation, and adjusting push method to handle squared values. Enhance clarity with updated constants for probe and records data sizes.

--- firmware/Periods.hpp
+++ firmware/Periods.hpp
@@ -7,43 +7,60 @@
 #include "Transformer.hpp"
 
 constexpr uint8_t RING_SIZE_LOG2 = 8;
-constexpr uint16_t RING_SIZE = 1UL << RING_SIZE_LOG2;
+constexpr uint16_t PROBE_DATA_SIZE = 1UL << RING_SIZE_LOG2;
+
+constexpr uint8_t RECORDS_SIZE_LOG2 = 5;
+constexpr uint16_t RECORDS_DATA_SIZE = 1UL << RECORDS_SIZE_LOG2;
 
 constexpr float FREQUENCY = 50;
 constexpr float CYCLE_TIME = 1000000 / FREQUENCY;
-constexpr uint16_t DELAY_MCS = CYCLE_TIME / RING_SIZE;
-
-char buffer[80];
+constexpr uint16_t DELAY_MCS = CYCLE_TIME / PROBE_DATA_SIZE;
 
 class Periods : public Tick {
+private:
+    uint16_t rms(const uint32_t* data, size_t length) {
+        uint32_t sum = 0;
+        for (size_t i = 0; i < length; i++) {
+            sum += data[i];
+        }
+        return (uint16_t)(sqrt(sum / length));
+    }
+
 public:
-    RingBuffer<uint16_t, RING_SIZE_LOG2> raw;
-    RingBuffer<Period, 5> periods;
+    RingBuffer<uint32_t, RING_SIZE_LOG2> raw;
+    RingBuffer<Period, RECORDS_SIZE_LOG2> records;
 
     uint32_t timestamp = 0;
 
     constexpr Periods() {}
 
-    void push(uint16_t value) {
-        if (value > 0) {
-            bool t = tick(value);
-            uint16_t flip = ticks & 0b11;
+    bool push(uint16_t value) {
+        bool t = tick(value);
+        uint16_t flip = ticks & 0b11;
 
-            if (t && flip == 0) {
-                uint32_t now = millis();
-                uint16_t duration = now - timestamp;
-                timestamp = now;
+        //////
+        uint32_t data = (((uint32_t)value * value));
+        /////
 
-                uint16_t rms = utils::rms(raw.data, raw.head);
+        if (t && flip == 0) {
+            uint32_t now = millis();
+            uint16_t duration = now - timestamp;
+            timestamp = now;
 
-                periods.push(Period(duration, rms));
+            uint16_t rms = Periods::rms(raw.data, raw.head);
 
-                sprintf(buffer, "\n%3d : pr=%3d >> raw=%4d | d=%3d | V=%3d", ticks, raw.head, rms, adc_to_direct(rms), adc_to_cV(rms));
-                Serial.println(buffer);
+            records.push(Period(duration, rms));
 
-                raw.rewind();
-            };
-            raw.push(value);
+
+
+            raw.rewind();
+            raw.push(data);
+            // raw.push(value);
+            return true;
+        } else {
+            raw.push(data);
+            // raw.push(value);
+            return false;
         }
     }
 };
