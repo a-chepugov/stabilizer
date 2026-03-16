dfd7630561b1b2707eba932984ee2c57b748bf00
a.chepugov <a.chepugov@gmail.com>
2026-03-16 15:40:26 +0200

    Refactor Transformer.h: Update constants for voltage reference and ADC calculations, improving clarity and accuracy. Replace efficiency with direct calculations for probe factors, enhancing maintainability of ADC to voltage conversions.

--- firmware/Transformer.h
+++ firmware/Transformer.h
@@ -30,13 +30,11 @@ constexpr float get_trans_factor(float k) {
 }
 
 constexpr float test_to_in_factor = input_k / test_k;
-
-// КПД трансформатора
-constexpr float efficiency = 0.98;
+constexpr uint16_t i_test_to_in_factor = test_to_in_factor;
 
 // Падение на диоде выпрямления
 constexpr float diode_drop = 0.613127;
-constexpr float diode_drop_cV = diode_drop * 100;
+constexpr uint16_t diode_drop_cV = diode_drop * 100;
 
 // Характеристики делителm напряжения
 constexpr float R1 = 42000;
@@ -49,28 +47,31 @@ constexpr uint16_t ADC_BITS = 10;
 constexpr uint16_t ADC_LEVELS = 1UL << ADC_BITS;
 constexpr uint16_t ADC_MAX_VALUE = ADC_LEVELS - 1;
 
-constexpr uint16_t VREF_V = 5;
-constexpr uint16_t VREF_cV = VREF_V * 100; // 5 В в сантивольты
+constexpr uint16_t VREF_cV = 500; // 5.00 В в сантивольтах
 
 ////////////////////////////////////////////////////////////////////////////////
 
 // Пересчёт показаний АЦП в сантивольты на датчике
 constexpr uint16_t adc_to_direct(uint16_t adc) {
-  return (((uint32_t)adc * VREF_cV) >> ADC_BITS);
+  return (((uint32_t)adc * VREF_cV) / ADC_MAX_VALUE);
 }
 
 // Коэффициент от датчика до входа
-constexpr float probe_to_in_factor = test_to_in_factor * divider_factor / efficiency;
-constexpr uint16_t probe_to_in_factor_full = probe_to_in_factor * VREF_cV;
+constexpr uint16_t probe_to_divider_factor = (float)VREF_cV * divider_factor * ADC_LEVELS / ADC_MAX_VALUE;
 
 // Пересчёт АЦП в сантивольты на входе трансформатора
 constexpr uint16_t adc_to_cV(uint16_t adc) {
-  return ((uint32_t)(adc) * probe_to_in_factor_full) >> ADC_BITS;
+  return (
+    (uint16_t)(
+      ((uint32_t)adc * probe_to_divider_factor) >> ADC_BITS
+    ) + diode_drop_cV
+  ) * i_test_to_in_factor
+  ;
 }
 
 // Пересчёт сантивольтов на входе трансформатора в показания АЦП
 constexpr uint16_t cV_to_adc(uint16_t cV) {
-  return ((uint32_t)cV << ADC_BITS) / probe_to_in_factor_full;
+  return ((uint32_t)(cV / i_test_to_in_factor - diode_drop_cV) << ADC_BITS) / probe_to_divider_factor;
 }
 
 constexpr float lspace = 0.43;
