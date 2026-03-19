d1c5569c6a83d87bf769ce6a0878786bc02b4395
a.chepugov <a.chepugov@gmail.com>
2026-03-19 17:19:59 +0200

    Refactor Box class: Change from struct to class, update constructor initialization, and rename operator() to map for clarity. Enhance operator overloading for assignment and mapping functionality.

--- firmware/models/Box.hpp
+++ firmware/models/Box.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
 template<typename T>
-struct Box {
-    T value;
+class Box {
+public:
+    const T value;
 
-    constexpr Box(T value): value(value) {}
+    constexpr Box(): value() {}
+    constexpr Box(T v): value(v) {}
 
-    constexpr get() { return value; }
+    constexpr Box operator=(const Box& other) const { return other; }
+
+    template<typename T2, typename Fn>
+    constexpr Box<T2> map(Fn fn) const { return Box<T2>(fn(value)); }
 
     template<typename T2, typename Fn>
     constexpr Box<T2> operator()(Fn fn) const { return Box<T2>(fn(value)); }
-
-    template<typename T2, typename Fn>
-    constexpr Box<T2> map(Fn fn) const {
-      return Box<T2>(fn(value));
-    }
 };
