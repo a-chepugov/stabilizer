cf53b4444d154926e6c6303865aa2681f5bc2a4a
a.chepugov <a.chepugov@gmail.com>
2026-03-19 17:20:19 +0200

    Refactor Period class: Change duration and rms members to const for immutability, and implement assignment operator for improved functionality and clarity.

--- firmware/models/Period.hpp
+++ firmware/models/Period.hpp
@@ -3,9 +3,11 @@
 
 class Period {
 public:
-    uint16_t duration;
-    uint16_t rms;
+    const uint16_t duration;
+    const uint16_t rms;
 
     constexpr Period() : duration(0), rms(0) {}
     constexpr Period(uint16_t duration, uint16_t rms) : duration(duration), rms(rms) {}
+
+    constexpr Period operator=(const Period& other) const { return other; }
 };
